cd5180b838b58a9d1806d9ff709d23546a0c6a69
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-06-30 09:25:13 +0200

    ajout premier twig

--- src/Taxes/Calculator.php
+++ src/Taxes/Calculator.php
@@ -4,6 +4,14 @@ namespace App\Taxes;
 
 class Calculator
 {
+    public function detect(float $number): bool
+    {
+        if ($number >= 100) {
+            return true;
+        }
+        return false;
+    }
+
     public function calculate(float $amount, float $taxRate): float
     {
         return $amount * (1 + $taxRate);
