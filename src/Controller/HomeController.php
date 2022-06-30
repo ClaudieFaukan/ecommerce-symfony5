d8f4e0472365eca2d8ebc21f2c34bdca6fe28021
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-06-30 10:27:31 +0200

    delete ext-php ds composer et ajout datafixtures orm

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -10,8 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function homepage()
+    public function homepage(EntityManagerInterface $em)
     {
+        $productRepository = $em->getRepository(Product::class);
+        $products = $productRepository->find(3);
+        dd($products);
+
         return $this->render('home.html.twig');
     }
 }
