ca796986f3609a608b030d87cddc4ad5ed9b45f3
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-07-21 09:53:21 +0200

    creation fixure user et entity User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
 use Liior\Faker\Prices;
 use App\Entity\Category;
@@ -10,17 +11,21 @@ use Bezhanov\Faker\Provider\Commerce;
 use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
     protected $slugger;
+    protected $passwordHasher;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $passwordHasher)
     {
         $this->slugger = $slugger;
+        $this->passwordHasher = $passwordHasher;
     }
 
+    //RAPPEL: lancer la commande de fixture avec php bin/console doctrine:fixtures:load
 
     public function load(ObjectManager $manager): void
     {
@@ -28,6 +33,26 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        //creation admin
+        $admin = new User;
+        $hash = $this->passwordHasher->hashPassword($admin, "admin");
+        $admin->setEmail('admin@admin.fr')
+            ->setFullName("admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
+
+        //Creation fake user
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+            $hash = $this->passwordHasher->hashPassword($user, 'password');
+            $user->setEmail("user$u@gmail.com");
+            $user->setPassword($hash);
+            $user->setFullName($faker->name);
+
+            $manager->persist($user);
+        }
 
         //Creation Fake Category
         for ($c = 0; $c < 3; $c++) {
