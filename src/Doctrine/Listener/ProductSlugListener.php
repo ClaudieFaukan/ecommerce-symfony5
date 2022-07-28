c525f8b88ff36a359ce416bdc93b43e09a74f1a9
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-07-28 14:53:58 +0200

    fds

--- src/Doctrine/Listener/ProductSlugListener.php
+++ src/Doctrine/Listener/ProductSlugListener.php
@@ -3,24 +3,26 @@
 namespace App\Doctrine\Listener;
 
 use App\Entity\Product;
-use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
-use Doctrine\Persistence\Event\LifecycleEventArgs as EventLifecycleEventArgs;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductSlugListener
 {
     protected $slugger;
+    protected $logger;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, LoggerInterface $logger)
     {
         $this->slugger = $slugger;
+        $this->logger = $logger;
     }
 
-    public function prePersist(Product $entity, EventLifecycleEventArgs $event)
+    public function prePersist(Product $entity)
     {
         if (empty($entity->getSlug())) {
             // SluggerInterface
             $entity->setSlug(strtolower($this->slugger->slug($entity->getName())));
         }
+        $this->logger->info("appeler");
     }
 }
