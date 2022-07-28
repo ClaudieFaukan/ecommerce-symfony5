644d37fe71b6e09dad6645ccd8a53f22e4e2ff37
Pierre-Alain <pierrealainpetrel@hotmail.fr>
2022-07-28 15:20:39 +0200

    alors peut etre

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,8 +9,6 @@ use Liior\Faker\Prices;
 use App\Entity\Category;
 use App\Entity\Purchase;
 use App\Entity\PurchaseItem;
-use App\Repository\CategoryRepository;
-use App\Repository\UserRepository;
 use Bezhanov\Faker\Provider\Commerce;
 use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Persistence\ObjectManager;
@@ -22,15 +20,11 @@ class AppFixtures extends Fixture
 {
     protected $slugger;
     protected $passwordHasher;
-    protected $userRepository;
-    protected $categoryRepository;
 
-    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $passwordHasher, UserRepository $userRepository, CategoryRepository $categoryRepository)
+    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $passwordHasher)
     {
         $this->slugger = $slugger;
         $this->passwordHasher = $passwordHasher;
-        $this->userRepository = $userRepository;
-        $this->categoryRepository = $categoryRepository;
     }
 
     //RAPPEL: lancer la commande de fixture avec php bin/console doctrine:fixtures:load
@@ -41,7 +35,7 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
-        /*
+
         //creation admin
         $admin = new User;
         $hash = $this->passwordHasher->hashPassword($admin, "admin");
@@ -61,7 +55,7 @@ class AppFixtures extends Fixture
             $hash = $this->passwordHasher->hashPassword($user, 'password');
             $user->setEmail("user$u@gmail.com");
             $user->setPassword($hash);
-            $user->setFullName($faker->name());
+            $user->setFullName($faker->name);
 
             //sert pour les fake purchases
             $users[] = $user;
@@ -69,45 +63,37 @@ class AppFixtures extends Fixture
             $manager->persist($user);
         }
 
-        //test debug
-        $users = $this->userRepository->findAll();
         //Creation Fake Category
         for ($c = 0; $c < 3; $c++) {
 
             $category = new Category();
-            $category->setName($faker->department());
-            $category->setSlug($this->slugger->slug(strtolower($category->getName())));
+            $category->setName($faker->department);
 
             $manager->persist($category);
 
-*/
-        //test
-        $category = $this->categoryRepository->find(11);
-        //Creation Fake products de cette categorie
-        $products = [];
-        for ($i = 0; $i < mt_rand(10, 20); $i++) {
-            $product = new Product();
-            $product->setName($faker->productName())
-                ->setPrice($faker->price(400, 2000))
-                ->setCategory($category)
-                ->setShortDescription($faker->paragraph())
-                ->setSlug("test")
-                ->setMainPicture($faker->imageUrl(400, 400, true));
+            //Creation Fake products de cette categorie
+            $products = [];
+            for ($i = 0; $i < mt_rand(10, 20); $i++) {
+                $product = new Product();
+                $product->setName($faker->productName)
+                    ->setPrice($faker->price(400, 200000))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph)
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
 
-            $manager->persist($product);
-            $products[] = $product;
+                $manager->persist($product);
+                $products[] = $product;
+            }
         }
-
-        /*
         //creation fake purchase
         for ($p = 0; $p < mt_rand(20, 40); $p++) {
 
             $purchase = new Purchase;
 
-            $purchase->setFullName($faker->name())
-                ->setAddress($faker->streetAddress())
-                ->setCity($faker->city())
-                ->setPostalCode($faker->postcode())
+            $purchase->setFullName($faker->name)
+                ->setAddress($faker->streetAddress)
+                ->setCity($faker->city)
+                ->setPostalCode($faker->postcode)
                 ->setUser($faker->randomElement($users))
                 ->setTotal(mt_rand(2000, 30000))
                 ->setPurchasedAt($faker->dateTimeBetween('-6 month'));
@@ -134,7 +120,7 @@ class AppFixtures extends Fixture
 
             $manager->persist($purchase);
         }
-        */
+
         $manager->flush();
     }
 }
